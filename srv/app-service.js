c2b81fee045fdf21a2787754f63080f13e6d1eb5
Siddharth Srivastava <sidsriv93@gmail.com>
2019-05-06 10:57:08 +0000

    Added UI Module

--- srv/app-service.js
+++ srv/app-service.js
@@ -8,30 +8,12 @@
  	// Pre Hooks
  	srv.before('CREATE', 'Airports', async(req) => {
  		const airport = req.data
- 		// return req.error(400, 'Not allowed right now.');
- 		// if (!order.amount || order.amount <= 0) return req.error(400, 'Order at least 1 book')
- 		// const tx = cds.transaction(req)
- 		// const affectedRows = await tx.run(
- 		// 	UPDATE(Books)
- 		// 	.set({
- 		// 		stock: {
- 		// 			'-=': order.amount
- 		// 		}
- 		// 	})
- 		// 	.where({
- 		// 		stock: {
- 		// 			'>=': order.amount
- 		// 		},
- 		// 		/*and*/
- 		// 		ID: order.book_ID
- 		// 	})
- 		// )
- 		// if (affectedRows === 0) req.error(409, "Sold out, sorry")
+
  	})
 
  	// Post Hooks
  	srv.after('READ', 'Airports', each => {
- 		each.Country = "Hellow World!";
+ 		
  	})
 
  }
